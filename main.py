ee7948a0acaeb2eaa088ec08e3487100b2298bd1
DEV CSM <121349256+hbr-dev@users.noreply.github.com>
2024-12-06 23:15:46 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
+from RPS import player
+from unittest import main
+
+play(player, quincy, 1000)
+print("===============================")
+print("===============================")
+play(player, abbey, 1000)
+print("===============================")
+print("===============================")
+play(player, kris, 1000)
+print("===============================")
+print("===============================")
+play(player, mrugesh, 1000)
+print("===============================")
+print("===============================")
